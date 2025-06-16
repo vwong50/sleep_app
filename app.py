9ef44c70003c9d8a6ac8ba3a50b85ce1f2fb3a77
vwong50 <vivian.wong50@lism.catholic.edu.au>
2025-06-16 18:16:50 +1000

    Figured it out a bit

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication
+from PyQt6 import QtWidgets, uic
+import sys
 
-Form, Window = uic.loadUiType("main.ui")
 
-app = QApplication([])
-window = Window()
-form = Form()
-form.setupUi(window)
-window.show()
-app.exec()
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Load the UI Page
+        uic.loadUi('main.ui', self)
+        self.nextPageButton.clicked.connect(self.new_user_button)
+
+    def new_user_button(self):
+        username = self.newNameInput.text()
+        password = self.newPasswordInput.text()
+        print(username)
+
+
+def app():
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    app()
