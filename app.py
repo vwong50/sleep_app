4f26d409304652a778fc0ab830fbddcaeb373835
vwong50 <vivian.wong50@lism.catholic.edu.au>
2025-06-16 16:36:58 +1000

    progress

--- app.py
+++ app.py
@@ -1,31 +1,11 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
-from PyQt6.QtCore import QSize, Qt
-import sys # For command line stuff, idk.
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication
 
-# Makes QMainWindow a subclass
-class MainWindow(QMainWindow):
-	def __init__(self):
-		super().__init__() # Initialises QMainWindow
-		self.setWindowTitle("Prototype") 	
-		self.button = QPushButton("Button")
+Form, Window = uic.loadUiType("main.ui")
 
-		# Signal
-		self.button.clicked.connect(self.button_clicked) 
-		self.setCentralWidget(self.button)
-		self.setMinimumSize(QSize(500, 500))
-
-	# Slot for button_clicked
-	def button_clicked(self):
-		# Updates the interface
-		self.button.setText("Clicked")
-		self.button.setEnabled(False)
-
-		self.setWindowTitle("Prototyp: Revamped")
-
-# Starts the app
-app = QApplication(sys.argv) # Starts app
-
-window = MainWindow()
-window.show() # Shows parent GUI
-
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
 app.exec()
