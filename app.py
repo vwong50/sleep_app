cf6d9c0a7def88885d3ba35dbb8247b20bc8e092
Sir0ne <vivian@hausofwong.com>
2025-06-20 18:27:44 +1000

    Made the sign in page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from PyQt6 import QtWidgets, uic
 import sys
+import sqlite3
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -8,12 +9,46 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Load the UI Page
         uic.loadUi('main.ui', self)
-        self.nextPageButton.clicked.connect(self.new_user_button)
+        self.pagesWidget.setCurrentIndex(1)
+        self.signUpBtn.clicked.connect(self.signUpBtn_pressed)
+        self.signInBtn.clicked.connect(self.signInBtn_pressed)
+        self.nextPageBtn1.clicked.connect(self.tab_1_btn)
+        self.nextPageBtn2.clicked.connect(self.tab_2_btn)
+        self.nextPageBtn3.clicked.connect(self.main_page_btn)
 
-    def new_user_button(self):
-        username = self.newNameInput.text()
-        password = self.newPasswordInput.text()
-        print(username)
+    def signUpBtn_pressed(self):
+        self.FormPage.setCurrentIndex(0)
+        self.pagesWidget.setCurrentIndex(0)
+
+    def signInBtn_pressed(self):
+        con = sqlite3.connect("users.db")
+        cur = con.cursor()
+        u_form = self.nameInput.text()
+        p_form = self.passwordInput.text()
+
+        u_db = cur.execute("SELECT name FROM users WHERE name=?", (u_form,)).fetchone()
+        p_db = cur.execute("SELECT password FROM users WHERE password=?", (p_form,)).fetchone()
+        if u_db is None or p_db is None:
+            return None
+        elif u_form == u_db[0] and p_form == p_db[0]:
+            self.pagesWidget.setCurrentIndex(2)
+        else:
+            pass
+
+    def tab_1_btn(self):
+        # Store username and password
+        self.username = self.newNameInput.text()
+        self.password = self.newPasswordInput.text()
+        self.FormPage.setCurrentIndex(1)
+
+    def tab_2_btn(self):
+        # Store sleep data
+        self.sleep_goal = self.goalInput.value()
+        self.sleep_amount = self.sleepInput.value()
+        self.FormPage.setCurrentIndex(2)
+
+    def main_page_btn(self):
+        self.pagesWidget.setCurrentIndex(1)
 
 
 def app():
